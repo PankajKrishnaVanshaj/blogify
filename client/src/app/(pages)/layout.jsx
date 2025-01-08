1b51ba36b11ff1ccbc0934a051015e8846e3fa47
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-01-09 01:31:11 +0530

    structured data error fix global

--- client/src/app/(pages)/layout.jsx
+++ client/src/app/(pages)/layout.jsx
@@ -3,15 +3,12 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { AuthProvider } from "@/context/AuthContext";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Toaster } from "sonner";
 
 export default function RootLayout({ children }) {
   const path = usePathname();
   const isDashboardPath = path.startsWith("/dashboard");
-
-  // State to track if the component has mounted (hydrated)
-  const [hasMounted, setHasMounted] = useState(false);
 
   // Function to retrieve token from URL and store it in localStorage
   const handleGoogleCallback = () => {
@@ -32,46 +29,13 @@ export default function RootLayout({ children }) {
 
   // Call `handleGoogleCallback` on component mount if you’re on the auth route
   useEffect(() => {
-    // Mark component as mounted (hydrated) after the first render
-    setHasMounted(true);
-
     if (window.location.pathname === "/auth") {
       handleGoogleCallback();
     }
   }, []);
 
-  const globalJsonLd = {
-    "@context": "https://schema.org",
-    "@type": "WebSite",
-    name: "PK Blogify",
-    url: "https://blogify.pankri.com",
-    description:
-      "PK Blogify is a dynamic social media blog platform designed to bring creators, writers, and readers together in one engaging space. Share your thoughts, publish compelling articles, and explore diverse topics across various categories. Whether you're here to follow your favorite bloggers, discover trending posts, or engage in meaningful discussions, PK Blogify offers a seamless and interactive experience tailored to the modern storyteller.",
-    potentialAction: {
-      "@type": "SearchAction",
-      target: "https://blogify.pankri.com/search?query={search_term_string}",
-      "query-input": "required name=search_term_string",
-    },
-  };
-
-  // Only render the layout after the component is mounted (hydrated) to avoid hydration errors
-  if (!hasMounted) {
-    return null; // Don't render anything during SSR
-  }
-
   return (
     <>
-      {/* Apply global JSON-LD script dynamically */}
-      <section>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify(globalJsonLd),
-          }}
-        />
-      </section>
-
-      {/* Render Navbar, Toaster, and Footer only if not on the dashboard route */}
       {!isDashboardPath && (
         <AuthProvider>
           <div className="w-full flex flex-col min-h-screen px-1.5 md:px-8 2xl:px-24">
--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -52,8 +52,30 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const globalJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: "PK Blogify",
+    url: "https://blogify.pankri.com",
+    description:
+      "PK Blogify is a dynamic social media blog platform designed to bring creators, writers, and readers together in one engaging space. Share your thoughts, publish compelling articles, and explore diverse topics across various categories. Whether you're here to follow your favorite bloggers, discover trending posts, or engage in meaningful discussions, PK Blogify offers a seamless and interactive experience tailored to the modern storyteller.",
+    potentialAction: {
+      "@type": "SearchAction",
+      target: "https://blogify.pankri.com/search?query={search_term_string}",
+      "query-input": "required name=search_term_string",
+    },
+  };
+
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(globalJsonLd),
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
