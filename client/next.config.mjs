a13a80194bc61a56e9ab5d996c9dc2e269b50644
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-01-10 22:35:04 +0530

    page speed

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
   images: {
     domains: ["server.blogify.pankri.com", "localhost"],
   },
-  experimental: {
-    optimizeCss: true, // Enable CSS optimization
-  },
-  webpack(config) {
-    // Ensure proper handling of CSS and JS files
-    config.optimization.splitChunks = {
-      chunks: 'all',
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
