c7651f85183961d009461a28c04956b4e8795450
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-01-15 23:18:25 +0530

    sitemap medias

--- client/src/app/sitemap.js
+++ client/src/app/sitemap.js
@@ -1,14 +1,23 @@
 import { fetchAllPosts } from "@/api/blogPost.api";
+import { fetchAllMedias } from "@/api/media.api";
 
 export default async function sitemap() {
   const posts = await fetchAllPosts();
+  const medias = await fetchAllMedias();
 
   const postUrls = posts.map((post) => ({
-    url: `${"https://blogify.pankri.com"}/${post._id.toString()}/post`, // Constructing the post URL
+    url: `${"https://blogify.pankri.com"}/${post._id.toString()}/post`, 
     lastModified: new Date(), // Use the post's last modified date
     changeFrequency: "daily", // Posts can change frequently
     priority: 0.5, // Default priority for posts
   }));
-  
-  return [...postUrls];
+
+  const mediaUrls = medias.map((media) => ({
+    url: `${"https://blogify.pankri.com"}/${media._id.toString()}/media`, 
+    lastModified: new Date(), // Use the media's last modified date
+    changeFrequency: "weekly", // Media might change less frequently
+    priority: 0.7, // Higher priority for media
+  }));
+
+  return [...postUrls, ...mediaUrls]; // Combine both post and media URLs
 }
