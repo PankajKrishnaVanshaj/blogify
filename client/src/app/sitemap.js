625e81bcd4663fe90e5b280e11a2053b350846e9
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-01-20 14:43:58 +0530

    sitemap error fix

--- client/src/app/sitemap.js
+++ client/src/app/sitemap.js
@@ -2,13 +2,11 @@ import { fetchAllPosts } from "@/api/blogPost.api";
 
 export default async function sitemap() {
   const posts = await fetchAllPosts();
-
   const postUrls = posts.map((post) => ({
-    url: `${"https://blogify.pankri.com"}/${post._id.toString()}/post`,
+    url: `${"https://blogify.pankri.com"}/${post._id.toString()}/post`, // Constructing the post URL
     lastModified: new Date(), // Use the post's last modified date
     changeFrequency: "daily", // Posts can change frequently
     priority: 0.5, // Default priority for posts
   }));
-
-  return [...postUrls]; // Combine both post and media URLs
+  return [...postUrls];
 }
